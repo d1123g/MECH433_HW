a858b5c39facbd2379b4391c66c90fa16053888a
d1123g <gonzad9104@gmail.com>
2025-04-24 13:16:26 -0500

    added in the blinks and also added an auto wrap feature.

--- HW7_I2C_OLED/HW7_I2C_OLED.c
+++ HW7_I2C_OLED/HW7_I2C_OLED.c
@@ -14,7 +14,7 @@
 
 void drawmytext(int x, int y, char *m);
 void drawLetter(int x, int y, char c);
-
+void pixelBlink(int x, int y);
 // have 128 pixels in a row, 32 pixels in a column
 // x,y coordinates are in the positive x direction and "negative" y direction
 // 0,0 is the top left corner of the screen
@@ -34,6 +34,10 @@ int main()
     gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+
+
 
     ssd1306_setup();
     ssd1306_clear();
@@ -41,42 +45,41 @@ int main()
 
     while (true) {
         char m[50];
-        sprintf(m, "hello");
+        sprintf(m, "Damian Gonzalez's HW7 I2C OLED"); // write the string to the buffer
         ssd1306_clear(); //just to make sure that it clears the screen before you write
         //if you wanted a hack you could do the following
         //sprintf(m, "hello       ");//this would write hello and then 7 spaces, which would effectively clear the screen
         drawmytext(2,7,m);
+        gpio_put(25, 1);
+        sleep_ms(100);
+        gpio_put(25, 0);
         ssd1306_update();
         //sometimes people update a certain part of the screen that is not necessary. sometimes there is a flicker
+        // pixelBlink(126, 30); // blink the pixel at the bottom right corner of the screen
+        // pixelBlink(127, 30); // blink the pixel at the bottom right corner of the screen
+        // pixelBlink(126, 31); // blink the pixel at the bottom right corner of the screen
+        pixelBlink(127, 31); // blink the pixel at the bottom right corner of the screen
         printf("check if it has written on OLED\n");
         sleep_ms(1000); // Update every 1 second
     }
 }
 
 void drawmytext(int x, int y, char *m) {
-    int i = 0;
-    while (m[i] != '\0') {
-        drawLetter(x + i*5, y, m[i]);
+    int i = 0; // index of the character in the string
+    int startX = x; // save the initial x position
+    while (m[i] != '\0') { // loop through every character in the string
+        if (x + 5 >= 128) {
+            x = startX;      // Reset to initial x
+            y += 8;          // Move to next row (each char is 8 pixels tall)
+            if (y + 8 > 32) break; // Don't draw below the screen
+        }
+        drawLetter(x, y, m[i]);
+        x += 6; // Move to next character (5 pixels + 1 spacing)
         i++;
     }
 }
 
-void drawLetter(int x, int y, char c) {
-    // i represents the row, j represents the column
-    //loop throught column in the font array
-    int i = 0;
-    int j = 0;
-    //
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 8; j++) {
-        
-            char col = ASCII[index][i];
-            char on = (col >> j) & 0b1;
-            ssd1306_drawPixel(x + i, y + j, on);
 
-        }
-    }
-}
 
 void drawLetter(int x, int y, char c) {
     int index = c - 0x20;
@@ -95,4 +98,12 @@ void drawLetter(int x, int y, char c) {
             ssd1306_drawPixel(x + i, y + j, on);
         }
     }
+}
+
+void pixelBlink(int x, int y) {
+    ssd1306_drawPixel(x, y, 1); // set the pixel to 1 (white)
+    ssd1306_update(); // update the screen
+    sleep_ms(100);
+    ssd1306_drawPixel(x, y, 0); // set the pixel to 0 (black)
+    ssd1306_update();  // update the screen
 }
